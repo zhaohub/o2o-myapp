487048cde3a884ebfbff0277643c5de58ab2ff8e
zhaogang <zg3407@gmail.com>
2016-09-18 18:24:43 +0800

    my atoi implementation

--- mytest-service/src/main/java/com/test/leetcode/StringtoInteger.java
+++ mytest-service/src/main/java/com/test/leetcode/StringtoInteger.java
@@ -7,9 +7,36 @@ public class StringtoInteger {
 
 
     public static int myAtoi(String str) {
+        if (str == null || str.length() == 0)
+            return 0;
+
+        boolean negative = false;
+        char sign = str.charAt(0);
+
+        if (sign == '-') {
+            negative = true;
+            str = str.substring(1, str.length());
+        } else if (sign == '+') {
+            negative = false;
+            str = str.substring(1, str.length());
+        }
+
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int c = str.charAt(i) - 48;
+            if (c > 10 || c < 0)
+                throw new NumberFormatException("For input string " + str);
+            result = result * 10 + c;
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
+                throw new NumberFormatException("For input string" + str);
+        }
+        return negative ? -result : result;
+    }
 
 
-        return 0;
+    public static void main(String[] args) {
+        String s = "892343";
+        System.out.println(myAtoi(s));
     }
 
 }
