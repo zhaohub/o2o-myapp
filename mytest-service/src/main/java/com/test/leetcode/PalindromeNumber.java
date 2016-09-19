a5ff578a73e12e8511ab816c69b6831d487eadca
zhaogang <zg3407@gmail.com>
2016-09-19 14:32:15 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/PalindromeNumber.java
@@ -0,0 +1,15 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/9/19.
+ */
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+
+
+        return false;
+    }
+}
