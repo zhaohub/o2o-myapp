177071ecb78baba8f4943d1f5e213c292651e9e3
zhaogang <zg3407@gmail.com>
2016-09-19 15:08:09 +0800

    implementation

--- mytest-service/src/main/java/com/test/leetcode/PalindromeNumber.java
+++ mytest-service/src/main/java/com/test/leetcode/PalindromeNumber.java
@@ -5,11 +5,28 @@ package com.test.leetcode;
  */
 public class PalindromeNumber {
 
+    /**
+     * 评论区中的解法
+     * r的位数最多比循环后的x多一位
+     *
+     * @param x
+     * @return
+     */
     public static boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
 
-
-
-        return false;
+        int r = 0;
+        while (x > r) {
+            x = x / 10;
+            r = r * 10 + x % 10;
+        }
+        return x == r || x == r / 10;
     }
+
+    public static void main(String[] args) {
+        int x = 12344321;
+        System.out.println(isPalindrome(x));
+    }
+
 }
