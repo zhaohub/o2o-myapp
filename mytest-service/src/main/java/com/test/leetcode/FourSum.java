922636aae90039071474725c3c8be883b5ff7615
zhaogang <zg3407@gmail.com>
2016-09-23 16:53:48 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/FourSum.java
@@ -0,0 +1,19 @@
+package com.test.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by zhaogang3 on 2016/9/23.
+ */
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums == null || nums.length < 4) return null;
+        Arrays.sort(nums);
+
+
+
+        return null;
+    }
+}
