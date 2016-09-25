16984663b9cf1720c31dff7f65a122c9074db5d5
zhaogang <zg3407@gmail.com>
2016-09-25 12:21:32 +0800

    merge two sorted lists

---
+++ mytest-service/src/main/java/com/test/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,78 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/9/25.
+ */
+public class MergeTwoSortedLists {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode tmpHead = new ListNode(0);
+        ListNode node = tmpHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                node.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                node.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        if (l1 == null) node.next = l2;
+        if (l2 == null) node.next = l1;
+
+        return tmpHead.next;
+    }
+
+    /**
+     * 评论区中的递归解法
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n22 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n31 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        ListNode n6 = new ListNode(6);
+
+        n1.next = n2;
+        n2.next = n22;
+        n22.next = n3;
+        n31.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        ListNode res = mergeTwoLists(n1, n31);
+    }
+}
