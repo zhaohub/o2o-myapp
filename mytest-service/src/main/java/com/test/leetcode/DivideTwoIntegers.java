3d8c441d5ba0409220dae76e52e30f959747d4d7
zhaogang <zg3407@gmail.com>
2016-09-26 20:59:32 +0800

    optimize

--- mytest-service/src/main/java/com/test/leetcode/DivideTwoIntegers.java
+++ mytest-service/src/main/java/com/test/leetcode/DivideTwoIntegers.java
@@ -33,8 +33,43 @@ public class DivideTwoIntegers {
         return sign ? i : -i;
     }
 
+    public static int divide2(int dividend, int divisor) {
+        if (divisor == 0)
+            throw new ArithmeticException("divied by zero");
+
+        if (dividend == 0) return 0;
+        if (Math.abs(dividend) < Math.abs(divisor)) return 0;
+        boolean sign = false;
+        if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0))
+            sign = true;
+
+        long dividendTmp = Math.abs(dividend);
+        long divisorTmp = Math.abs(divisor);
+
+        long c = 1;
+        while (dividendTmp > divisorTmp) {
+            divisorTmp = divisorTmp << 1;
+            c = c << 1;
+        }
+
+        int res = 0;
+        while (dividendTmp >= Math.abs(divisor)) {
+            while (dividendTmp >= divisorTmp) {
+                dividendTmp -= divisorTmp;
+                res += c;
+            }
+            divisorTmp = divisorTmp >> 1;
+            c = c >> 1;
+        }
+        return sign ? res : -res;
+    }
+
     public static void main(String[] args) {
-        System.out.println(divide(-10, 3));
-        System.out.println(-10 / 3);
+        long start = System.currentTimeMillis();
+        System.out.println(divide(1523535523, 1));
+        System.out.println("time1:" + (System.currentTimeMillis() - start));
+        long end = System.currentTimeMillis();
+        System.out.println(divide2(1523535523, 1));
+        System.out.println("time2:" + (System.currentTimeMillis() - end));
     }
 }
