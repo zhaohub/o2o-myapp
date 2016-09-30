ed8170b9714b922230e1b1b4decb9f5605d67957
zhaogang <zg3407@gmail.com>
2016-09-30 10:48:38 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/LongestValidParentheses.java
@@ -0,0 +1,15 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/9/29.
+ */
+public class LongestValidParentheses {
+
+    public static int longestValidParentheses(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+
+
+        return 0;
+    }
+}
