58c5c2d2797207a71b4ef197120e78568a6b87fa
zhaogang <zg3407@gmail.com>
2016-10-04 08:25:35 +0800

    valid sudoku

---
+++ mytest-service/src/main/java/com/test/leetcode/ValidSudoku.java
@@ -0,0 +1,40 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/10/3.
+ */
+public class ValidSudoku {
+
+    public static boolean isValidSudoku(char[][] board) {
+        int[][] times = new int[10][10];
+        int[][] subTimes = new int[10][10];
+
+        for (int i = 0; i < board.length; i++) {
+            char[] row = board[i];
+            for (int j = 0; j < row.length; j++) {
+                if (row[j] != '.') {
+                    int d = row[j];
+                    if (++times[i][d] > 1 || ++subTimes[j / 3 + i / 3 * 3][d] > 1)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {5, 3, '.', '.', 7, '.', '.', '.', '.'},
+                {6, '.', '.', 1, 9, 5, '.', '.', '.'},
+                {'.', 9, 8, '.', '.', '.', '.', 6, '.'},
+                {8, '.', '.', '.', 6, '.', '.', '.', 3},
+                {4, '.', '.', 8, '.', 3, '.', '.', 1},
+                {7, '.', '.', '.', 2, '.', '.', '.', 6},
+                {'.', 6, '.', '.', '.', '.', 2, 5, '.'},
+                {'.', '.', '.', 4, 1, 9, '.', '.', 5},
+                {'.', '.', '.', '.', 8, '.', '.', 7, 9}
+        };
+        System.out.println(isValidSudoku(board));
+    }
+
+}
