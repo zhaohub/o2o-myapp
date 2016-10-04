9255fc0e8dab011a4cdad596a78ed291eae1c09e
zhaogang <zg3407@gmail.com>
2016-10-04 08:36:43 +0800

    sudoku solver

---
+++ mytest-service/src/main/java/com/test/leetcode/SudokuSolver.java
@@ -0,0 +1,12 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/10/4.
+ */
+public class SudokuSolver {
+
+    public static void solveSudoku(char[][] board) {
+
+        
+    }
+}
