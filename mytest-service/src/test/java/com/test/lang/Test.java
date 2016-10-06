257f870d5f1ccaab714d95e5feca5f1f825f2e18
zhaogang <zg3407@gmail.com>
2016-10-06 23:03:47 +0800

    add pow test

--- mytest-service/src/test/java/com/test/lang/Test.java
+++ mytest-service/src/test/java/com/test/lang/Test.java
@@ -1,5 +1,6 @@
 package com.test.lang;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,13 +11,17 @@ import java.util.Map;
 public class Test {
 
     public static void main(String[] args) {
-        Map map = new HashMap();
+        /*Map map = new HashMap();
         map.put("test1", "test1");
         map.put("test2", "test2");
         MyMap myMap = new MyMap(map.entrySet());
 
         System.out.println(myMap.get("test1"));
-        System.out.println(myMap.containsKey("test1"));
+        System.out.println(myMap.containsKey("test1"));*/
+
+        BigInteger bigInteger = new BigInteger("9");
+        System.out.println(bigInteger.pow(81).toString());
+        //196627050475552913618075908526912116283103450944214766927315415537966391196809
 
     }
 }
