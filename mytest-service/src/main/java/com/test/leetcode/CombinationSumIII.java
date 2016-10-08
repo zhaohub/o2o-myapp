46fa38f7279646731f3b21b8e266cc8f36b8e8ed
zhaogang <zg3407@gmail.com>
2016-10-08 22:10:45 +0800

    combination sum III

---
+++ mytest-service/src/main/java/com/test/leetcode/CombinationSumIII.java
@@ -0,0 +1,16 @@
+package com.test.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by zhaogang3 on 2016/10/8.
+ */
+public class CombinationSumIII {
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+
+
+
+        return null;
+    }
+}
