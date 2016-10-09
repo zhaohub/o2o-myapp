1c2c99841bfef423a884861fcddf8845006e1ab4
zhaogang <zg3407@gmail.com>
2016-10-09 17:57:17 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/FirstMissingPositive.java
@@ -0,0 +1,13 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/10/9.
+ */
+public class FirstMissingPositive {
+
+    public static int firstMissingPositive(int[] nums) {
+
+        return 0;
+    }
+
+}
