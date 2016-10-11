f2ad5ca3cab398e70b0314b057eddfa3231581a0
zhaogang <zg3407@gmail.com>
2016-10-11 09:11:41 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/TrappingRainWater.java
@@ -0,0 +1,14 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/10/11.
+ */
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+
+        
+        return 0;
+    }
+
+}
