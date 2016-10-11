21608f5aa7cd332629f0e1f1e73ae37e91ce8457
zhaogang <zg3407@gmail.com>
2016-10-11 23:07:15 +0800

    trapping rain water

--- mytest-service/src/main/java/com/test/leetcode/TrappingRainWater.java
+++ mytest-service/src/main/java/com/test/leetcode/TrappingRainWater.java
@@ -6,9 +6,30 @@ package com.test.leetcode;
 public class TrappingRainWater {
 
     public static int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int res = 0, maxLeft = 0, maxRight = 0;
 
-        
-        return 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft)
+                    maxLeft = height[left];
+                else
+                    res += maxLeft - height[left];
+                left++;
+            } else {
+                if (height[right] >= maxRight)
+                    maxRight = height[right];
+                else
+                    res += maxRight - height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] h = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trap(h));
     }
 
 }
