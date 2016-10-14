aa535135f6efa233529afa2167686e3df2ec822b
zhaogang <zg3407@gmail.com>
2016-10-14 10:06:27 +0800

    wildcard matching

--- mytest-service/src/main/java/com/test/leetcode/WildcardMatching.java
+++ mytest-service/src/main/java/com/test/leetcode/WildcardMatching.java
@@ -7,39 +7,63 @@ public class WildcardMatching {
 
     public static boolean isMatch(String s, String p) {
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-
         dp[0][0] = true;
 
         for (int i = 0; i < p.length(); i++) {
-            if (p.charAt(i) == '*' && dp[0][i - 1])
-                dp[0][i + 1] = true;
+            if (p.charAt(i) == '*')
+                dp[s.length()][i] = true;
+            else
+                break;
         }
 
         for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j < p.length(); j++) {
                 if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))
                     dp[i + 1][j + 1] = dp[i][j];
+                else if (p.charAt(j) == '*')
+                    dp[i + 1][j + 1] = dp[i][j + 1] || dp[j][i + 1];
+                else
+                    dp[i + 1][j + 1] = false;
+            }
+        }
 
-                if (p.charAt(j) == '*') {
-                    if (s.charAt(i) != p.charAt(j + 1))
-                        dp[i + 1][j + 1] = dp[i][j];
-                    else
-                        dp[i + 1][j + 1] = dp[i][j + 1] || dp[i - 1][j + 1];
-                }
-            }
-        }
-        for (int i = 0; i < dp.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < dp[0].length; j++) {
-                sb.append(dp[i][j] + " ");
-            }
-            System.out.println(sb.toString());
-        }
         return dp[s.length()][p.length()];
+    }
+
+    /**
+     * better solution,easy understand
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public static boolean isMatch2(String s, String p) {
+        int sp = 0, pp = 0, match = 0, startId = -1;
+        while (sp < s.length()) {
+            if (pp < p.length() && (s.charAt(sp) == p.charAt(pp) || p.charAt(pp) == '?')) {
+                sp++;
+                pp++;
+            } else if (pp < p.length() && p.charAt(pp) == '*') {
+                startId = pp;
+                match = sp;
+                pp++;
+            } else if (startId != -1) {
+                pp = startId + 1;   //recover pp pointer
+                match++;
+                sp = match;
+            } else
+                return false;
+        }
+
+        while (pp < p.length() && p.charAt(pp) == '*')
+            pp++;
+
+        return pp == p.length();
     }
 
     public static void main(String[] args) {
         //System.out.println(isMatch("aeuwyetur", "?*uuu"));
-        System.out.println(isMatch("abmnc", "ab*c"));
+        //System.out.println(isMatch("abmdddnc", "ab*c"));
+        System.out.println(isMatch2("abmdddncqqqqq", "ab*cq***"));
     }
 }
