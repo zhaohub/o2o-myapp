3df361fd891b09bd896a037f52ee809096b8e8d4
zhaogang <zg3407@gmail.com>
2016-10-17 23:10:55 +0800

    fix pow xn

--- mytest-service/src/main/java/com/test/leetcode/PowXN.java
+++ mytest-service/src/main/java/com/test/leetcode/PowXN.java
@@ -7,9 +7,9 @@ public class PowXN {
 
     public static double myPow(double x, int n) {
         if (n == 0) return 1;
-        if (n == 1) return x;
 
-        int t = 2;
+        int t = 2, tn = n;
+        n = Math.abs(n);
         double tx = x;
         if (n >= 2) {
             while (t <= n) {
@@ -18,20 +18,23 @@ public class PowXN {
             }
             x = x * myPow(tx, n - (t >> 1));
         }
-        return x;
+
+        return tn > 0 ? x : 1 / x;
     }
 
     public static double myPow2(double x, int n) {
+        int tn = n;
+        n = Math.abs(n);
         double res = 1;
         for (int i = 1; i <= n; i++) {
             res *= x;
         }
-        return res;
+        return tn > n ? res : 1 / res;
     }
 
     public static void main(String[] args) {
-        System.out.println(myPow(5.1, 122));
-        System.out.println(myPow2(5.1, 122));
-        System.out.print(Math.pow(5.1, 122));
+        System.out.println(myPow(5.1, -122));
+        System.out.println(myPow2(5.1, -122));
+        System.out.print(Math.pow(5.1, -122));
     }
 }
