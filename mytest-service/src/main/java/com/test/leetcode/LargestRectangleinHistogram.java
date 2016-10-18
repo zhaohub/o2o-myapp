ec82692e8d088269bd8c22293b2ecfb5d5705ac1
zhaogang <zg3407@gmail.com>
2016-10-18 23:17:26 +0800

    my version

--- mytest-service/src/main/java/com/test/leetcode/LargestRectangleinHistogram.java
+++ mytest-service/src/main/java/com/test/leetcode/LargestRectangleinHistogram.java
@@ -14,18 +14,53 @@ public class LargestRectangleinHistogram {
         int n = heights.length, i = 0, maxArea = 0;
         while (i < n) {
             if (stack.empty() || heights[i] >= heights[stack.peek()])
+                stack.push(i);
+            else {
+                while (!stack.empty() && heights[stack.peek()] > heights[i]) {
+                    int top = stack.pop();
+                    int area = heights[top] * (stack.empty() ? i : i - stack.peek() - 1);
+                    maxArea = maxArea > area ? maxArea : area;
+                }
+                stack.push(i);
+            }
+            i++;
+        }
+
+        while (!stack.empty()) {
+            int top = stack.pop();
+            int area = heights[top] * (stack.empty() ? i : i - stack.peek() - 1);
+            maxArea = Math.max(area, maxArea);
+        }
+
+        return maxArea;
+    }
+
+
+    /**
+     * perfect version
+     *
+     * @param heights
+     * @return
+     */
+    public static int largestRectangleArea1(int[] heights) {
+        if (heights == null || heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int n = heights.length, i = 0, maxArea = 0;
+        while (i < n) {
+            if (stack.empty() || heights[i] >= heights[stack.peek()])
                 stack.push(i++);
             else {
                 int top = stack.pop();
                 int area = heights[top] * (stack.empty() ? i : i - stack.peek() - 1);
-                maxArea = maxArea > area ? maxArea : area;
+                maxArea = Math.max(area, maxArea);
             }
         }
 
         while (!stack.empty()) {
             int top = stack.pop();
             int area = heights[top] * (stack.empty() ? i : i - stack.peek() - 1);
-            maxArea = maxArea > area ? maxArea : area;
+            maxArea = Math.max(area, maxArea);
         }
 
         return maxArea;
@@ -34,6 +69,7 @@ public class LargestRectangleinHistogram {
     public static void main(String[] args) {
         int[] heights = {2, 1, 5, 6, 2, 3, 4, 6, 6, 2, 1};
         int[] h1 = {2, 1, 5, 6, 2, 3};
-        System.out.println(largestRectangleArea(h1));
+        System.out.println(largestRectangleArea(heights));
+        System.out.println(largestRectangleArea1(heights));
     }
 }
