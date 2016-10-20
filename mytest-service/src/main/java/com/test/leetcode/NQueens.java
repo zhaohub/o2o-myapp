c9163ef5a7d445a4c425675ef979321c19ed0714
zhaogang <zg3407@gmail.com>
2016-10-20 12:56:12 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/NQueens.java
@@ -0,0 +1,14 @@
+package com.test.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by zhaogang3 on 2016/10/20.
+ */
+public class NQueens {
+
+    public List<List<String>> solveNQueens(int n) {
+
+        return null;
+    }
+}
---
+++ mytest-service/src/main/java/com/test/leetcode/NQueensII.java
@@ -0,0 +1,13 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/10/20.
+ */
+public class NQueensII {
+
+    public int totalNQueens(int n) {
+
+        return 0;
+    }
+
+}
