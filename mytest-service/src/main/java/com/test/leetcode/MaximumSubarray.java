2f7f1393f830a5ea86ab12f770c1cedba47ffa25
zhaogang <zg3407@gmail.com>
2016-10-22 21:28:28 +0800

    another solution

--- mytest-service/src/main/java/com/test/leetcode/MaximumSubarray.java
+++ mytest-service/src/main/java/com/test/leetcode/MaximumSubarray.java
@@ -26,9 +26,33 @@ public class MaximumSubarray {
         return maxSum;
     }
 
+    /**
+     * o(n)solution
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray1(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] > 0 ? dp[i - 1] + nums[i] : nums[i];
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
+
     public static void main(String[] args) {
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums = {-2, 1, -5, 4, -1, 2, 1, -3, 4};
         System.out.println(maxSubArray(nums));
+        System.out.println(maxSubArray1(nums));
     }
 
 
