5298dea9ac8713f599dfc469c75549b57f42ab3a
zhaogang <zg3407@gmail.com>
2016-10-29 21:44:01 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/TestJustification.java
@@ -0,0 +1,22 @@
+package com.test.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by zhaogang3 on 2016/10/29.
+ */
+public class TestJustification {
+
+    public static List<String> fullJustify(String[] words, int maxWidth) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        List<String> res = fullJustify(words, 16);
+
+    }
+
+}
