ed144dae5f5fe6573ca6363b34e133ed095cce9e
zhaogang <zg3407@gmail.com>
2016-11-01 13:57:06 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/Sqrtx.java
@@ -0,0 +1,16 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/10/31.
+ */
+public class Sqrtx {
+
+    public int mySqrt(int x) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
