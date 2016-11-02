744b7fb6f3a23540dc3a2242cbbe4ddd41fcd95e
zhaogang <zg3407@gmail.com>
2016-11-02 22:09:51 +0800

    set matrix zeros

---
+++ mytest-service/src/main/java/com/test/leetcode/SetMatrixZeroes.java
@@ -0,0 +1,55 @@
+package com.test.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by zhaogang3 on 2016/11/2.
+ */
+public class SetMatrixZeroes {
+
+    public static void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        List<int[]> list = new ArrayList<int[]>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    int[] temp = {i, j};
+                    list.add(temp);
+                }
+            }
+        }
+
+        for (int[] a : list) {
+            for (int t = 0; t < n; t++)
+                matrix[a[0]][t] = 0;
+
+            for (int t = 0; t < m; t++)
+                matrix[t][a[1]] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3, 5, 1},
+                {2, 2, 0, 2, 2},
+                {2, 3, 4, 5, 6}
+        };
+
+        setZeroes(matrix);
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+}
