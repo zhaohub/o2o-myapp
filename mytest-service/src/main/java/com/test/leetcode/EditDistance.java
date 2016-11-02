a32cf1b7fc835d2e2819a87ecf4666a45cc7cf3d
zhaogang <zg3407@gmail.com>
2016-11-02 22:10:14 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/EditDistance.java
@@ -0,0 +1,20 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/11/2.
+ */
+public class EditDistance {
+
+    public static int minDistance(String word1, String word2) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
