2668515831641b7d5058565fe5cb4e4f3ef0f7dc
zhaogang <zg3407@gmail.com>
2016-11-03 09:55:01 +0800

    set matrix zeroes

--- mytest-service/src/main/java/com/test/leetcode/SetMatrixZeroes.java
+++ mytest-service/src/main/java/com/test/leetcode/SetMatrixZeroes.java
@@ -35,15 +35,40 @@ public class SetMatrixZeroes {
         }
     }
 
+
+    public static void setZeroes1(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int col0 = 1;
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if (col0 == 0)
+                matrix[i][0] = 0;
+        }
+    }
+
+
     public static void main(String[] args) {
 
         int[][] matrix = {
-                {1, 2, 3, 5, 1},
-                {2, 2, 0, 2, 2},
+                {1, 2, 0, 5, 1},
+                {2, 2, 1, 2, 2},
                 {2, 3, 4, 5, 6}
         };
 
-        setZeroes(matrix);
+        setZeroes1(matrix);
         printMatrix(matrix);
     }
 
