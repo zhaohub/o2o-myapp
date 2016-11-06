544214132acf417a317795b9a1b21ae06375ee00
zhaogang <zg3407@gmail.com>
2016-11-06 22:30:37 +0800

    sort colors

--- mytest-service/src/main/java/com/test/leetcode/SortColors.java
+++ mytest-service/src/main/java/com/test/leetcode/SortColors.java
@@ -1,16 +1,35 @@
 package com.test.leetcode;
 
+import java.util.Arrays;
+
 /**
  * Created by zhaogang3 on 2016/11/5.
  */
 public class SortColors {
 
     public static void sortColors(int[] nums) {
-
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j > 0) {
+                if (nums[j] > nums[j - 1])
+                    break;
+                else if (nums[j] < nums[j - 1])
+                    swap(nums, j - 1, j);
+                j--;
+            }
+        }
     }
 
-    public static void main(String[] args){
-
+    private static void swap(int[] nums, int m, int n) {
+        int t = nums[m];
+        nums[m] = nums[n];
+        nums[n] = t;
     }
 
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 2, 0, 2, 0, 0, 1, 2, 1, 1, 0, 0, 1, 0, 2, 0, 0};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 }
