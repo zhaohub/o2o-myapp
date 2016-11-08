f9911e5866c00ed8bd0c5eeb422d265e0e46c19e
zhaogang <zg3407@gmail.com>
2016-11-08 19:42:52 +0800

    minimum window substring

--- mytest-service/src/main/java/com/test/leetcode/MinimumWindowSubstring.java
+++ mytest-service/src/main/java/com/test/leetcode/MinimumWindowSubstring.java
@@ -1,15 +1,58 @@
 package com.test.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by zhaogang3 on 2016/11/7.
  */
 public class MinimumWindowSubstring {
 
     public static String minWindow(String s, String t) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (char c : s.toCharArray()) {
+            map.put(c, 0);
+        }
+        for (char c : t.toCharArray()) {
+            if (map.containsKey(c))
+                map.put(c, map.get(c) + 1);
+            else
+                return "";
+        }
 
+        int start = 0, end = 0, minStart = 0, minEnd = Integer.MAX_VALUE, counter = t.length();
 
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            if (map.get(c) > 0)
+                counter--;
+            map.put(c, map.get(c) - 1);
 
-        return null;
+            end++;
+
+            while (counter == 0) { //find a substring
+                if (minEnd - minStart > end - start) {
+                    minEnd = end;
+                    minStart = start;
+                }
+
+                char cc = s.charAt(start);
+                map.put(cc, map.get(cc) + 1);
+
+                if (map.get(cc) > 0)
+                    counter++;
+
+                start++;
+            }
+        }
+
+        return minEnd == Integer.MAX_VALUE ? "" : s.substring(minStart, minEnd);
     }
 
+
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println(minWindow(s, t));
+    }
 }
