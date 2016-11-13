d10a03ab5a515602a07fdf77ff3991d9396eff2a
zhaogang <zg3407@gmail.com>
2016-11-13 21:06:20 +0800

    fix decode ways

--- mytest-service/src/main/java/com/test/leetcode/DecodeWays.java
+++ mytest-service/src/main/java/com/test/leetcode/DecodeWays.java
@@ -6,20 +6,28 @@ package com.test.leetcode;
 public class DecodeWays {
 
     public static int numDecodings(String s) {
+        if (s == null || s.length() == 0) return 0;
         char p = s.charAt(0);
-        int i = 1, count = 1;
+
+        int[] count = new int[s.length() + 1];
+        count[0] = 1;
+        count[1] = Integer.parseInt(s.substring(0, 1)) <= 26 ? 1 : 0;
+
+        int i = 1;
         while (i < s.length()) {
             char cur = s.charAt(i);
             int t = Character.digit(p, 10) * 10 + Character.digit(cur, 10);
             if (t <= 26)
-                count++;
+                count[i + 1] = count[i] + count[i - 1];
+            else
+                count[i + 1] = count[i];
             i++;
             p = cur;
         }
-        return count;
+        return count[s.length()];
     }
 
     public static void main(String[] args) {
-        System.out.println(numDecodings("12294"));
+        System.out.println(numDecodings("1229123"));
     }
 }
