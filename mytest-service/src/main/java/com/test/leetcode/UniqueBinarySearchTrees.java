838f77851e027d8c6855b569d9c080e3895c1969
zhaogang3 <zhaogang3@jd.com>
2016-11-17 11:56:01 +0800

    unique binary search trees

---
+++ mytest-service/src/main/java/com/test/leetcode/UniqueBinarySearchTrees.java
@@ -0,0 +1,26 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/11/15.
+ */
+public class UniqueBinarySearchTrees {
+
+    public static int numTrees(int n) {
+
+        int[] g = new int[n + 1];
+        g[0] = g[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                g[i] += g[j - 1] * g[i - j];
+            }
+        }
+
+        return g[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numTrees(3));
+    }
+
+}
