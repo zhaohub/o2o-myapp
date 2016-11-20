9da8771d5b52ffef05eb15bdf5d00e270a0b8d17
zhaogang <zg3407@gmail.com>
2016-11-20 22:32:31 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/ValidateBinarySearchTree.java
@@ -0,0 +1,25 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/11/20.
+ */
+public class ValidateBinarySearchTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+
+        return false;
+    }
+}
+
+
+
