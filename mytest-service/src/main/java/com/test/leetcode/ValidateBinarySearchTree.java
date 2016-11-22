5567c761f8d5a02b2aba13449df10fe03da6804d
zhaogang <zg3407@gmail.com>
2016-11-22 15:21:07 +0800

    valid binary search tree

--- mytest-service/src/main/java/com/test/leetcode/ValidateBinarySearchTree.java
+++ mytest-service/src/main/java/com/test/leetcode/ValidateBinarySearchTree.java
@@ -15,14 +15,30 @@ public class ValidateBinarySearchTree {
         }
     }
 
-    public boolean isValidBST(TreeNode root) {
+    public static boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
 
-        if(root == null)
+    private static boolean isValidBST(TreeNode root, TreeNode minNode, TreeNode maxNode) {
+        if (root == null) return true;
+
+        if (minNode != null && minNode.val >= root.val || maxNode != null && maxNode.val <= root.val)
             return false;
 
-
-        return false;
+        return isValidBST(root.left, minNode, root) && isValidBST(root.right, root, maxNode);
     }
+
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        n1.left = n2;
+        n1.right = n3;
+        System.out.println(isValidBST(n1));
+
+    }
+
 }
 
 
