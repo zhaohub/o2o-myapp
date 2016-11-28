a8721c806f30e247d613a6a1e4b68b789f654121
zhaogang <zg3407@gmail.com>
2016-11-28 20:01:45 +0800

    recover binary search tree

--- mytest-service/src/main/java/com/test/leetcode/RecoverBinarySearchTree.java
+++ mytest-service/src/main/java/com/test/leetcode/RecoverBinarySearchTree.java
@@ -5,7 +5,7 @@ package com.test.leetcode;
  */
 public class RecoverBinarySearchTree {
 
-    public class TreeNode {
+    static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -15,14 +15,44 @@ public class RecoverBinarySearchTree {
         }
     }
 
+    private static TreeNode first, second, pre = new TreeNode(Integer.MIN_VALUE);
+
     public static void recoverTree(TreeNode root) {
+        traverse(root);
+        int tmp = first.val;
+        first.val = second.val;
+        second.val = tmp;
 
+    }
 
+    private static void traverse(TreeNode root) {
+        if (root == null) return;
+        traverse(root.left);
+        if (first == null && root.val <= pre.val)
+            first = pre;
+
+        if (first != null && root.val <= pre.val)
+            second = root;
+
+        pre = root;
+        traverse(root.right);
     }
 
 
     public static void main(String[] args) {
-
-
+        TreeNode root = new TreeNode(4);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(6);
+        TreeNode n3 = new TreeNode(3);
+        n2.left = n1;
+        n2.right = n3;
+        root.left = n2;
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(2);
+        TreeNode n7 = new TreeNode(7);
+        n6.left = n5;
+        n6.right = n7;
+        root.right = n6;
+        recoverTree(root);
     }
 }
