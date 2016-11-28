e304be206ef18bffd6251405fc9829b2822385af
zhaogang <zg3407@gmail.com>
2016-11-28 20:17:24 +0800

    optimize code

--- mytest-service/src/main/java/com/test/leetcode/SameTree.java
+++ mytest-service/src/main/java/com/test/leetcode/SameTree.java
@@ -18,8 +18,8 @@ public class SameTree {
     public static boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
 
-        if ((p != null && q != null) && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
-            return true;
+        if ((p != null && q != null) && p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
         return false;
     }
