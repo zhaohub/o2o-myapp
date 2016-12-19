72dbe4bbf0c1edb85cc5fcf23568678e070a5ee6
zhaogang <zg3407@gmail.com>
2016-12-19 20:26:33 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/ConvertSortedListtoBinarySearchTree.java
@@ -0,0 +1,32 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2016/12/19.
+ */
+public class ConvertSortedListtoBinarySearchTree {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode sortedListToBST(ListNode head) {
+
+        return null;
+    }
+
+}
