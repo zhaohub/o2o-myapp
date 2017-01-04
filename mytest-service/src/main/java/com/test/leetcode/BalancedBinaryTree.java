c9ef201e81014ffb4e543429e3710399b8b28c46
zhaogang <zg3407@gmail.com>
2017-01-04 20:48:29 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/BalancedBinaryTree.java
@@ -0,0 +1,24 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2017/1/4.
+ */
+public class BalancedBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+
+
+        return false;
+    }
+
+}
