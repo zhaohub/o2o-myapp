fe0a3c3f0baac606cedd616324d587061a47aff6
zhaogang <zg3407@gmail.com>
2017-01-05 17:48:06 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/PathSum.java
@@ -0,0 +1,23 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2017/1/5.
+ */
+public class PathSum {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+
+        return false;
+    }
+
+}
