a6e32e39b4c4f99a0c598e833b9579dfb967efa3
zhaogang <zg3407@gmail.com>
2017-01-05 18:24:28 +0800

    path sum

--- mytest-service/src/main/java/com/test/leetcode/PathSum.java
+++ mytest-service/src/main/java/com/test/leetcode/PathSum.java
@@ -16,8 +16,37 @@ public class PathSum {
     }
 
     public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
 
-        return false;
+        if (root.left == null && root.right == null)
+            return sum == root.val;
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(5);
+        TreeNode n2 = new TreeNode(4);
+        TreeNode n3 = new TreeNode(8);
+        TreeNode n4 = new TreeNode(11);
+        TreeNode n5 = new TreeNode(13);
+        TreeNode n6 = new TreeNode(4);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(2);
+        TreeNode n9 = new TreeNode(1);
+
+        n1.left = n2;
+        n1.right = n3;
+
+        n2.left = n4;
+        n4.left = n7;
+        n4.right = n8;
+
+        n3.left = n5;
+        n3.right = n6;
+        n6.right = n9;
+        System.out.println(hasPathSum(n1, 22));
     }
 
 }
