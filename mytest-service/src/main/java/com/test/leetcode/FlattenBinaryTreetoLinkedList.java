5b51a41a906eed72bba55805b34cd01ac56f4900
zhaogang <zg3407@gmail.com>
2017-01-09 18:22:06 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,22 @@
+package com.test.leetcode;
+
+/**
+ * Created by zhaogang3 on 2017/1/9.
+ */
+public class FlattenBinaryTreetoLinkedList {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public void flatten(TreeNode root) {
+
+
+    }
+}
