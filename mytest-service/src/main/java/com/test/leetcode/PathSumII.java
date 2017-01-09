cd329951ed2539b8239fc1c6fbe36e96ecf98b4f
zhaogang <zg3407@gmail.com>
2017-01-09 17:16:24 +0800

    add file

---
+++ mytest-service/src/main/java/com/test/leetcode/PathSumII.java
@@ -0,0 +1,27 @@
+package com.test.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by zhaogang3 on 2017/1/9.
+ */
+public class PathSumII {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+
+
+        return null;
+    }
+
+}
