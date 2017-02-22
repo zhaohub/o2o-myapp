3b7ca51bac692b54be903f09c674c0685a6cb9e8
zhaogang <zg3407@gmail.com>
2017-02-22 15:13:21 +0800

    web config

--- myapp-web/src/main/java/com/test/controller/TestController.java
+++ myapp-web/src/main/java/com/test/controller/TestController.java
@@ -1,8 +1,29 @@
 package com.test.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by zhaogang3 on 2016/9/18.
  */
+@Controller
+@RequestMapping("/test")
 public class TestController {
+
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String test(HttpServletRequest request, HttpServletResponse response){
+
+        System.out.println(request.getParameter("hello"));
+
+
+        return "1111";
+    }
+
 
 }
