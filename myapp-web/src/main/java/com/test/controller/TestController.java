6e140f92b75635c92a893670018178fabfa0e173
zhaogang3 <zhaogang3@jd.com>
2017-02-24 16:23:49 +0800

    修改

--- myapp-web/src/main/java/com/test/controller/TestController.java
+++ myapp-web/src/main/java/com/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +16,18 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/test")
 public class TestController {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
 
     @RequestMapping("hello")
     @ResponseBody
     public String test(HttpServletRequest request, HttpServletResponse response){
+        LOGGER.info("TestController->test");
 
         System.out.println(request.getParameter("hello"));
 
 
-        return "1111";
+        return "TestController";
     }
 
 
