26d60f922b5c2950492ef24dc691a59038346b23
zhaogang <zg3407@gmail.com>
2017-04-04 10:54:04 +0800

    test load

--- myapp-service/src/test/java/com/test/threadpool/ThreadPoolTest.java
+++ myapp-service/src/test/java/com/test/threadpool/ThreadPoolTest.java
@@ -1,38 +1,1 @@
-package com.test.threadpool;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-/**
- * Created by zhaogang3 on 2017/2/13.
- */
-public class ThreadPoolTest {
-
-
-    public static void main(String[] args) throws InterruptedException {
-
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 1000000; i++) {
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    for(int j=0;j<10000000;j++){
-                        try {
-                            //Thread.sleep(100L);
-                            Math.sin(j);
-                            //System.out.println("hello"+Thread.currentThread().getName());
-                        } catch (Exception e
-                            ) {
-                            //e.printStackTrace();
-                        }
-                    }
-                }
-            });
-        }
-        executorService.awaitTermination(1, TimeUnit.DAYS);
-
-        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-    }
-}
+package com.test.threadpool;import java.util.concurrent.ExecutorService;import java.util.concurrent.Executors;import java.util.concurrent.ScheduledExecutorService;import java.util.concurrent.TimeUnit;/** * Created by zhaogang3 on 2017/2/13. */public class ThreadPoolTest {    public static void main(String[] args) throws InterruptedException {        ExecutorService executorService = Executors.newFixedThreadPool(10);        for (int i = 0; i < 1000000; i++) {            executorService.execute(new Runnable() {                @Override                public void run() {                    for(int j=0;j<10000000;j++){                        try {                            //Thread.sleep(100L);                            Math.sin(j);                            //System.out.println("hello"+Thread.currentThread().getName());                        } catch (Exception e                            ) {                            //e.printStackTrace();                        }                    }                }            });        }        executorService.awaitTermination(1, TimeUnit.DAYS);        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();    }}
