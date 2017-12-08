f872c64a0fa30845c29a1ae2481cdc4afdc9eb8d
zhaogang3 <zhaogang3@jd.com>
2017-12-08 18:26:22 +0800

    base64某些情况下要好于urlencoder

--- myapp-service/src/test/java/com/test/lang/HashMapTest.java
+++ myapp-service/src/test/java/com/test/lang/HashMapTest.java
@@ -1,6 +1,11 @@
 package com.test.lang;
 
 
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,13 +16,42 @@ public class HashMapTest {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<String>();
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+        /*for (int i = 0; i < Integer.MAX_VALUE; i++) {
             String t = String.valueOf(i).intern();
             list.add(t);
-        }
+        }*/
         /*System.setProperty("user.timezone","GMT +01");
         long time = new Date().getTime();
         //System.out.println(new Date().getTimezoneOffset());
         System.out.println(time/1000%86400/3600);*/
+
+        //System.out.println(StringUtils.remove(UUID.randomUUID().toString(),"-").toUpperCase());
+
+        String s = null;
+
+        try {
+            byte[] s1111 = Base64.encodeBase64("aaaaaabbbbbbb".getBytes());
+            System.out.println(new String(s1111));
+
+            s1111 = Base64.decodeBase64(s1111);
+
+            System.out.println(new String(s1111));
+
+            System.out.println("--------------");
+
+            s = URLEncoder.encode("aaaaaabbbbbbb", "utf8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(s);
+        try {
+            System.out.println(URLDecoder.decode(s, "utf8"));
+
+            String ss = "春节";
+            byte[] bs = ss.getBytes("utf8");
+            System.out.println();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
--- myapp-web/src/main/java/com/test/controller/TestController.java
+++ myapp-web/src/main/java/com/test/controller/TestController.java
@@ -2,7 +2,6 @@ package com.test.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.ConfigurableObjectInputStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,19 +25,19 @@ public class TestController {
 
     @RequestMapping("hello")
     @ResponseBody
-    public Map<String, Object> test(HttpServletRequest request, HttpServletResponse response){
+    public Map<String, Object> test(HttpServletRequest request, HttpServletResponse response) {
         LOGGER.info("TestController->test");
         //response.setCharacterEncoding("utf-8");
         System.out.println(request.getParameter("hello"));
         String s = "{\"type\":\"0\",\"code\":\"redirect\",\"msg\":\"重定向\",\"result\":\"https://baidu.com\",\"detail\":\"\",\"success\":false}";
 
 
-        Map<String,Object> map = new HashMap<String, Object>();
-        map.put("type",0);
-        map.put("code","redirect");
-        map.put("msg","重定向");
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("type", 0);
+        map.put("code", "redirect");
+        map.put("msg", "重定向");
 
-        response.addHeader("Set-Cookie","PDJ_H5_MOBILE123=152****4045; Version=1; Max-Age=86400; Expires=Tue, 01-Aug-2017 09:28:44 GMT; Path=/");
+        response.addHeader("Set-Cookie", "PDJ_H5_MOBILE123=152****4045; Version=1; Max-Age=86400; Expires=Tue, 01-Aug-2017 09:28:44 GMT; Path=/");
 
         try {
             response.sendRedirect("http://www.126.com");
@@ -48,5 +47,9 @@ public class TestController {
         return map;
     }
 
+    @RequestMapping("hello1")
+    public void test1(HttpServletRequest request, HttpServletResponse response) {
 
+        response.addCookie(new Cookie("aaaa","bbbbb"));
+    }
 }
